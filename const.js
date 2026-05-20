7fdd952075751c93c011ff0879c52d3fa0819e7b
ImJamie <jamie@929design.co.uk>
2026-05-20 23:34:50 +0100

    feat: Externalize configuration using dotenv
    Moves sensitive credentials and bot settings from hardcoded values in `const.js` to a `.env` file. This improves security by keeping secrets out of version control and simplifies environment-specific configuration. The `.env` file is also added to `.gitignore`.

--- const.js
+++ const.js
@@ -1,14 +1,14 @@
-const CHANNEL_NAMES = ['#imjammietbh', '#lytzsifer'];
-const OAUTH_TOKEN = 'oauth:71rs9qmix5nrw8amfcvkosm6lm9dl5';
-const BOT_USERNAME = 'MakingThatDev';
+require('dotenv').config();
 
-const CLIENT_ID = 'gvbwpe9aqpe8i2su8o50d4er314rxj';
-const CLIENT_SECRET = '390lpxpwwynexh93041t4nuq7hf81n';
+const CHANNEL_NAMES = process.env.CHANNEL_NAMES.split(',');
+const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
+const BOT_USERNAME = process.env.BOT_USERNAME;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 module.exports = {
     CHANNEL_NAMES,
     BOT_USERNAME,
     CLIENT_ID,
     CLIENT_SECRET,
-    OAUTH_TOKEN,
-}
+    OAUTH_TOKEN };
